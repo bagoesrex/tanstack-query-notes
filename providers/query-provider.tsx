ea446dce62b050bfa375f079df27dbac86aea4e5
bagoesrex <bagoesrex@gmail.com>
2025-11-01 17:06:53 +0700

    feat: integrate react query devtools for development mode only

--- providers/query-provider.tsx
+++ providers/query-provider.tsx
@@ -2,11 +2,13 @@
 
 import { queryClient } from "@/lib/query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
         </QueryClientProvider >
     )
 }

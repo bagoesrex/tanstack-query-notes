41dc87aa2241ce853c123ab3916a8aeb2933df1d
bagoesrex <bagoesrex@gmail.com>
2025-11-11 21:24:10 +0700

    feat: add MDX support and update layout/page configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-"use client"
-
-import { useEffect } from "react"
-import { useCharacters } from "@/hooks/use-characters"
+import HomeMdx from '@/content/home.mdx'
 
 export default function Home() {
-  const { fetchStatus, isPending, error, data } = useCharacters()
-
-  useEffect(() => {
-    console.log("Fetch status changed:", fetchStatus)
-  }, [fetchStatus])
-
-  if (isPending) return 'Loading...'
-
-  if (error) return 'An error has occurred: ' + error.message
-
-  return (
-    <div>
-      <h1>{data?.characters[4]?.name}</h1>
-      <p>Fetch Status: {fetchStatus}</p>
-    </div>
-  )
+    return (
+        <main className="min-h-screen max-w-xl w-full mx-auto">
+            <HomeMdx />
+        </main>
+    )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["mdx", "ts", "tsx"],
   reactCompiler: true,
 };
 
-export default nextConfig;
+const withMDX = createMDX({})
+
+export default withMDX(nextConfig);

48282f5ccca9290237735b7545bd7076bb5f2116
bagoesrex <bagoesrex@gmail.com>
2025-11-12 21:27:10 +0700

    feat: add notes layout and update notes page rendering

---
+++ app/notes/layout.tsx
@@ -0,0 +1,11 @@
+export default function NotesLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <section className="min-h-screen max-w-xl w-full mx-auto mt-12">
+            {children}
+        </section>
+    )
+}
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,15 +4,17 @@ export default function NotesPage() {
     const notes = getAllMdxMetadata()
 
     return (
-        <section className="min-h-screen max-w-xl w-full mx-auto">
-            <h1 className="font-semibold text-xl mt-12 tracking-tighter">Notes</h1>
-            {notes.map((note) => (
-                <div key={note.slug}>
-                    <p>{note.title}</p>
-                    <p>{note.slug}</p>
-                    <p>{note.createdAt}</p>
-                </div>
-            ))}
-        </section>
+        <>
+            <h1 className="font-semibold text-xl tracking-tighter">Notes</h1>
+            {
+                notes.map((note) => (
+                    <div key={note.slug}>
+                        <p>{note.title}</p>
+                        <p>{note.slug}</p>
+                        <p>{note.createdAt}</p>
+                    </div>
+                ))
+            }
+        </>
     )
 }

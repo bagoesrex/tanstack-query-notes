49fabe47c572c3665fdd8016b5431fae9e5d66ec
bagoesrex <bagoesrex@gmail.com>
2025-11-12 22:13:45 +0700

    feat: update content and add notes section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import HomeMdx from '@/content/queries.mdx'
+import NoteCard from "@/components/notes/note-card"
+import { getAllMdxMetadata } from "@/lib/mdx-utils"
+import Link from "next/link"
 
 export default function Home() {
+    const notes = getAllMdxMetadata()
+
     return (
-        <main className="min-h-screen max-w-xl w-full mx-auto">
-            <HomeMdx />
+        <main className="min-h-screen max-w-xl w-full mx-auto mt-12">
+            <section id="hero">
+                <h1 className="font-semibold text-xl tracking-tighter mb-5">My TanStack Query Journey | Bagoes Rex</h1>
+                <p className="mb-5">These are my personal notes from exploring TanStack Query. Hopefully, they’ll help others who are on the same learning journey.</p>
+            </section>
+            <section id="notes">
+                <Link href="/notes" className="block mb-3 w-fit border-b-2 border-dashed hover:border-solid">
+                    Notes
+                </Link>
+                <div className="flex flex-col gap-1.5">
+                    {notes
+                        .sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+                        .slice(0, 3)
+                        .map((note) => (
+                            <NoteCard key={note.slug} note={note} />
+                        ))}
+                </div>
+            </section>
         </main>
     )
 }

dd4177ec25441078cd4d72a23cd72cbef53bd8ab
bagoesrex <bagoesrex@gmail.com>
2025-11-14 22:19:02 +0700

    feat: add back link to notes in notes detail

---
+++ app/notes/[slug]/layout.tsx
@@ -0,0 +1,25 @@
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function NotesDetailLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="relative">
+            <div className="sticky top-0 z-50 flex justify-end p-2">
+                <Link
+                    href="/notes"
+                    className="flex flex-row items-center gap-2 px-2 py-0.5 text-md rounded border border-dashed hover:border-solid bg-white"
+                >
+                    <ArrowLeft size={17} />
+                    Back to notes
+                </Link>
+            </div>
+            <div className="-mt-9">
+                {children}
+            </div>
+        </div>
+    )
+}
